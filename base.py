87e46647d63cbcd5423e6acf819b56102de9312c
Everton Mota <tom.mota.de.assis33@gmail.com>
2022-12-02 21:32:15 -0300

    Quarto commit

--- base.py
+++ base.py
@@ -3,37 +3,6 @@ import pygame, time, random
 
 from pygame.locals import *
 
-def main():
-    # variáveis do jogo
-    quit = False
-    x = 0
-    y = 0
-    # inicia o looping do jogo
-    while not quit:
-        # muda a cor da tela para azul
-        window.fill((135, 206, 235)) 
-        # eventos
-        keyspressed = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                quit = True
-            if event.type == KEYDOWN:
-                if event.key == ord("a"):
-                    x = x - 50
-                if event.key == ord("d"):
-                    x = x + 50
-                if event.key == ord("w"):
-                    y = y - 50
-                if event.key == ord("s"):
-                    y = y + 50
-            
-        player = Rect(x, y, 50, 50)
-        pygame.draw.rect(window, (204, 0, 255), player)
-
-        # atualiza a tela
-        pygame.display.update()
-        clock.tick(25)
-
 def main2():
     # variáveis do jogo
     player_stand = pygame.image.load("assets/pixil-frame-bro.png").convert_alpha()
@@ -43,7 +12,7 @@ def main2():
     y = 0
     while not quit:
         # muda a cor da tela para azul
-        window.fill((135, 206, 235)) 
+        window.fill((135, 206, 235))
         # eventos
         keyspressed = pygame.key.get_pressed()
         for event in pygame.event.get():
